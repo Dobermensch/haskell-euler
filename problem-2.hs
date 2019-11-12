d1dda46235f872257b507d4550b4baf882019803
Ayudh <ayudh_5@hotmail.com>
2019-11-12 21:19:20 +0800

    mod: problem-2

--- problem-2.hs
+++ problem-2.hs
@@ -1,5 +1,6 @@
--- run: func [1,2] 2 4000000 
-func arr cur limit
-    | cur < limit = func (arr ++ addition:[]) addition limit
-    | cur >= limit = sum $ filter even arr
-  where addition = arr !! (length arr - 1) + arr !! (length arr - 2)  
+-- run: func [1,2] 4000000 
+func arr limit  
+    | lastArr < limit = func (arr ++ addition:[]) limit
+    | lastArr >= limit = sum $ filter even arr
+  where addition = arr !! (length arr - 1) + arr !! (length arr - 2)
+        lastArr = last arr  
